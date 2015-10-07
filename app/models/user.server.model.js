90d3747a167a64c19243f56dbc8fea4658a37466
Igor Ilic <ilicigor@gmail.com>
2015-10-07 10:57:56 +0200

    zavrsen passport

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,78 +1,115 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Load the module dependencies
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+	crypto = require('crypto'),
+	Schema = mongoose.Schema;
 
+// Define a new 'UserSchema'
 var UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: {
-        type: String,
-        index: true,
-        match: /.+\@.+\..+/
-    },
-    username: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        validate: [
-            function(password) {
-                return password.length >= 6;
-            },
-            'Pass should be longer than 6 chars'
-        ]
-    },
-    website: {
-        type: String,
-        get: function(url) {
-            if (!url) {
-                return url;
-            } else {
-                if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
-                    url = 'http://' + url;
-                }
+	firstName: String,
+	lastName: String,
+	email: {
+		type: String,
+		// Validate the email format
+		match: [/.+\@.+\..+/, "Please fill a valid email address"]
+	},
+	username: {
+		type: String,
+		// Set a unique 'username' index
+		unique: true,
+		// Validate 'username' value existance
+		required: 'Username is required',
+		// Trim the 'username' field
+		trim: true
+	},
+	password: {
+		type: String,
+		// Validate the 'password' value length
+		validate: [
 
-                return url;
-            }
-        }
-    },
-    role: {
-        type: String,
-        enum: ['Admin', 'Owner', 'User']
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    }
+			function(password) {
+				return password && password.length > 6;
+			}, 'Password should be longer'
+		]
+	},
+	salt: {
+		type: String
+	},
+	provider: {
+		type: String,
+		// Validate 'provider' value existance
+		required: 'Provider is required'
+	},
+	providerId: String,
+	providerData: {},
+	created: {
+		type: Date,
+		// Create a default 'created' value
+		default: Date.now
+	}
 });
 
-UserSchema.post('save', function(next) {
-    if (this.isNew) {
-        console.log('A new user was created');
-    } else {
-        console.log('A user has updated details');
-    }
-});
-
+// Set the 'fullname' virtual property
 UserSchema.virtual('fullName').get(function() {
-    return this.firstName + ' ' + this.lastName;
+	return this.firstName + ' ' + this.lastName;
 }).set(function(fullName) {
-    var splitName = fullName.split(' ');
-    this.firstName = splitName[0] || '';
-    this.lastName = splitName[1] || '';
+	var splitName = fullName.split(' ');
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
 });
 
-UserSchema.statics.findOneByUsername = function(username, callback) {
-    this.findOne({
-        username: new RegExp(username, 'i')
-    }, callback);
+// Use a pre-save middleware to hash the password
+UserSchema.pre('save', function(next) {
+	if (this.password) {
+		this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+		this.password = this.hashPassword(this.password);
+	}
+
+	next();
+});
+
+// Create an instance method for hashing a password
+UserSchema.methods.hashPassword = function(password) {
+	return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
 };
 
+// Create an instance method for authenticating user
 UserSchema.methods.authenticate = function(password) {
-    return this.password === password;
+	return this.password === this.hashPassword(password);
 };
 
-UserSchema.set('toJSON', { getters: true, virtuals: true });
+// Find possible not used username
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+	var _this = this;
+
+	// Add a 'username' suffix
+	var possibleUsername = username + (suffix || '');
+
+	// Use the 'User' model 'findOne' method to find an available unique username
+	_this.findOne({
+		username: possibleUsername
+	}, function(err, user) {
+		// If an error occurs call the callback with a null value, otherwise find find an available unique username
+		if (!err) {
+			// If an available unique username was found call the callback method, otherwise call the 'findUniqueUsername' method again with a new suffix
+			if (!user) {
+				callback(possibleUsername);
+			} else {
+				return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+			}
+		} else {
+			callback(null);
+		}
+	});
+};
+
+// Configure the 'UserSchema' to use getters and virtuals when transforming to JSON
+UserSchema.set('toJSON', {
+	getters: true,
+	virtuals: true
+});
+
+// Create the 'User' model out of the 'UserSchema'
 mongoose.model('User', UserSchema);
--- config/express.js
+++ config/express.js
@@ -1,41 +1,61 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Load the module dependencies
 var config = require('./config'),
-    express = require('express'),
+	express = require('express'),
 	morgan = require('morgan'),
 	compress = require('compression'),
 	bodyParser = require('body-parser'),
 	methodOverride = require('method-override'),
-    session = require('express-session');
+	session = require('express-session'),
+	flash = require('connect-flash'),
+	passport = require('passport');
 
-
+// Define the Express configuration method
 module.exports = function() {
+	// Create a new Express application instance
 	var app = express();
 
-
+	// Use the 'NDOE_ENV' variable to activate the 'morgan' logger or 'compress' middleware
 	if (process.env.NODE_ENV === 'development') {
 		app.use(morgan('dev'));
 	} else if (process.env.NODE_ENV === 'production') {
 		app.use(compress());
 	}
 
+	// Use the 'body-parser' and 'method-override' middleware functions
 	app.use(bodyParser.urlencoded({
 		extended: true
 	}));
 	app.use(bodyParser.json());
 	app.use(methodOverride());
 
-    app.use(session({
-        saveUninitialized: true,
-        resave: true,
-        secret: config.sessionSecret
-    }));
+	// Configure the 'session' middleware
+	app.use(session({
+		saveUninitialized: true,
+		resave: true,
+		secret: config.sessionSecret
+	}));
 
-    app.set('views', './app/views');
-    app.set('view engine', 'ejs');
+	// Set the application view engine and 'views' folder
+	app.set('views', './app/views');
+	app.set('view engine', 'ejs');
 
-	require('../app/routes/index.server.routes')(app);
-    require('../app/routes/user.server.routes')(app);
+	// Configure the flash messages middleware
+	app.use(flash());
 
-    app.use(express.static('./public'));
+	// Configure the Passport middleware
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-	return(app);
-}
+	// Load the routing files
+	require('../app/routes/index.server.routes.js')(app);
+	require('../app/routes/users.server.routes.js')(app);
+
+	// Configure static file serving
+	app.use(express.static('./public'));
+
+	// Return the Express application instance
+	return app;
+};
